54484012356233fb3368b05318aa886c14e358a8
Macel B <macel.6b@gmail.com>
2025-05-16 16:41:37 -0600

    Creación de 5 registros en tabla productos

--- inserciones.sql
+++ inserciones.sql
@@ -5,6 +5,16 @@ INSERT INTO usuarios VALUES (null, "Fernando Bartolome", "fbartolomem.98@gmail.c
 INSERT INTO usuarios VALUES (null, "Uriel Medina", "urielmedina@ciencias.unam.mx", "Nabopo12.", "5613129854");
 INSERT INTO usuarios VALUES (null, "Eduardo Esteva Sarralde", "eestevass@gmail.com", "Enero2001#", "5519135105");
 
+INSERT INTO productos VALUES (null, "Arnés", 1799, "Arnés de seguridad ergonómico para escalada deportiva.", "escalada", "https://images.arcteryx.com/F24/1350x1710/Skaha-Harness-Lampyre-Boxcar.jpg");
+INSERT INTO productos VALUES (null, "Cuerda Dinámica", 3499, "Cuerda para absorber impactos y caídas en escalada.", "escalada", "https://www.teufelberger.com/media/catalog/product/cache/ad06d83191629f33f6df1b655e73fe3f/a/p/apex.jpg");
+INSERT INTO productos VALUES (null, "Mosquetón con seguro", 349, "Mosquetón con cierre automático para mayor seguridad.", "escalada", "https://m.media-amazon.com/images/I/51JMBbFK-dL._AC_.jpg");
+INSERT INTO productos VALUES (null, "Casco", 1299, "Casco resistente para proteger tu cabeza en todo momento.", "escalada", "https://m.media-amazon.com/images/I/61zWMe28UlL._AC_SL1500_.jpg");
+INSERT INTO productos VALUES (null, "Gatas", 2199, "Zapatos de precisión y comodidad para escalar mejor.", "escalada", "https://contents.mediadecathlon.com/p2613846/1cr1/k$e33abc7dddd7a50e86c3b848a927624e/gatas-escalada-gris-cometa.jpg?format=auto&f=768x0");
 
 
-SELECT * from usuarios;
+
+
+SELECT * FROM usuarios;
+SELECT * FROM productos;
+
+
